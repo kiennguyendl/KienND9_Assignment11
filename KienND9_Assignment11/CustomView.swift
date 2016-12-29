2ca3a456182c14e74c39738de6b0165af61947cd
Kien Nguyen Dang <kiennguyendang@Kiens-Mac.local>
2016-12-29 14:56:13 +0700

    first commit

---
+++ KienND9_Assignment11/CustomView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomView.swift
+//  KienND9_Assignment11
+//
+//  Created by Kien Nguyen Dang on 11/28/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import Foundation
